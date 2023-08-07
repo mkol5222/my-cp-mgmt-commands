9b95d7710a477559712f41f6f78008c71ae42634
mkol5222 <mkoldov@gmail.com>
2023-08-07 14:55:38 +0000

    new commands, build bins

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMANDS="approve_session  login discard  install_policy  logout  publish  reject_session  submit_session  verify_policy"
+COMMANDS="approve_session show_access_rulebase show_sessions show_packages login discard  install_policy  logout  publish  reject_session  submit_session  verify_policy"
 
 mkdir -p /workspaces/my-cp-mgmt-commands/releases/windows-amd64
 mkdir -p /workspaces/my-cp-mgmt-commands/releases/linux-amd64
